b559e9dddd495d0f766a43e11e737cd7646eb126
DaffaHQ <93602072+DaffaHQ@users.noreply.github.com>
2021-11-17 22:34:33 +0700

    Add files via upload

---
+++ No 5 perhitungan.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+ int angka1,angka2;
+
+ cout<<"Membuat Perhitungan"<<endl;
+ cout<<"Masukan Angka Pertama = ";
+ cin>>angka1;
+ cout<<"Masukan Angka Kedua = ";
+ cin>>angka2;
+ cout<<"Hasil Penjumlahan = "<<angka1+angka2<<endl;
+ cout<<"Hasil Pengurangan = "<<angka1-angka2<<endl;
+ cout<<"Hasil Perkalian = "<<angka1*angka2<<endl;
+ cout<<"Hasil Pembagian = "<<angka1/angka2<<endl;
+
+ return 0;
+}
---
+++ no 4 menghitung umur.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+int main (){
+    int tgllhr,blnlhr,thlhr;
+    char nama [30];
+    printf("masukan nama anda :"); scanf("%s",&nama);
+    printf("masukan tanggal kelahiran anda :"); scanf("%d",&tgllhr);
+    printf("masukan bulan kelahiran anda :"); scanf("%d",&blnlhr);
+    printf("masukan tahun kelahiran anda :"); scanf("%d",&thlhr);
+    printf("Hai  %s !! \n",nama);
+    printf("usia anda saat ini : %d tahun \ ",2021-thlhr); printf("%d bulan \ ",10-blnlhr); printf("%d hari \n================================================",9-tgllhr);
+    return 0;
+}
+
